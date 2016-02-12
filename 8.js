6aec9cc9cd3c62192fbc5419b153dfc19095999d
tfarino-efc <tanya.farino@gmail.com>
2016-02-12 22:25:31 +0300

    8.js

--- 8.js
+++ 8.js
@@ -1,26 +1,21 @@
 <!DOCTYPE html>
 <html>
-
-  <head>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
-  </head>
-
   <body>
     <script>
     'use strict';
-var beer=8;
-var str='';
-var str1=' bottles of beer on the wall, ';
-var str2=' bottles of beer.'+'\n'+' Take one down and pass it around,';
-var str3='No more bottles of beer on the wall, no more bottles of beer.'+'\n'+'Go to the store and buy some more, '+beer+' bottles of beer on the wall.'
-  
-for (var i=beer; i>=1; i--) {  
-    str=str+ i + str1 + i + str2;
-    str=str+'\n'  // отображение каждой строки с новой строки
-}
-str=str+str3;
-alert (str);
+    
+      var beer = prompt ('Numbers of bottles of beer', '');
+      var str = '';
+      var str1 = ' bottles of beer on the wall, ';
+      var str2 = ' bottles of beer.'+'\n'+' Take one down and pass it around,';
+      var str3 = 'No more bottles of beer on the wall, no more bottles of beer.'+'\n'+'Go to the store and buy some more, '+beer+' bottles of beer on the wall.'
+        
+      for (var i = beer; i >= 1; i--) {  
+          str = str+ i + str1 + i + str2;
+          str = str +'\n'  // отображение каждой строки с новой строки
+      }
+      str = str + str3;
+      alert (str);
 </script>
   </body>
 

bec486cd78dedd3c3c6ff37cc76c9bcd881ef696
tfarino-efc <tanya.farino@gmail.com>
2016-02-12 21:40:32 +0300

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,25 +1,19 @@
 <!DOCTYPE html>
 <html>
 
-  <head>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
-  </head>
-
   <body>
     <script>
     'use strict';
-
-    //Написать функцию, которая на входе получает число, а на выходе возвращает 
-    //массив из 3-х boolean элиментов, где первый - это является ли число нечетным, 
-    //второй - является ли оно простым, и, наконец, делится ли число на 3.
-   var a, x, y, z;
+    //a function determines:
+    //1.whether a number is odd;
+    //2.whether it is simple;
+    //3.whether the number is divisible by 3.
+    
+   var a = 3;
+   var x, y, z;
 
     function getMassive (a) {
 //1    
-     a = prompt('Введите число', '');
-      
-      
       if (a % 2) {
         x = true;
       } else {
@@ -52,7 +46,6 @@
       } 
       
       var arr = [x,y,z];
-    
     
      return ('является ли '+a+' нечетным - '+arr[0]+', является ли '+a+' простым - '+arr[1] +', делится ли '+a+' на 3 без остатка - '+arr[2]);
 }

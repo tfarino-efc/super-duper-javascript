db787ff9d0772ee25651655aefd03fb82eb07adf
tfarino-efc <tanya.farino@gmail.com>
2016-02-14 19:59:33 +0200

    8.js

---
+++ 8.js
@@ -0,0 +1,34 @@
+<html> 
+<head> 
+<script language = 'javascript'> 
+'use strict'
+
+
+function uniqueElements(phrase) {
+  var phrase;
+  var unique = [];
+
+  //Создание метки для continue
+  outer:
+    for (var i = 0; i < phrase.length; i++) {
+      var element = phrase[i];
+      
+      //Поиск одинаковых элементов
+      for (var j = 0; j < unique.length; j++) {
+        if (unique[j] == element) {
+          continue outer;
+        }
+      }
+      
+      unique.push(element);
+    }
+
+  return unique;
+}
+
+console.log(uniqueElements('123454dffssssss'));
+
+</script> 
+<body></body> 
+</html>
+

8c2aa18baf41ff5b31fc1c22189b768bfe68fa11
tfarino-efc <tanya.farino@gmail.com>
2016-02-16 11:30:20 +0300

    11.js

---
+++ 11.js
@@ -0,0 +1,31 @@
+<html> 
+<head> 
+<script language = 'javascript'> 
+'use strict'
+
+function contains(where, what) {
+  var check = [];
+    
+  for (var i = 0; i < what.length; i++) {
+    for (var j = 0; j < where.length; j++) {
+      if (what[i] == where[j]) {
+          check.push(what[i]);
+      }
+    }
+  }
+  
+  for (var k = 0; k < where.length; k++) {
+    if((check[k]!==what[k])||(check.length!==what.length)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+console.log(contains([4, 3, 1, 2, 5, 6], [1,2,4,3,1,2,5,6,6,5,3]));
+
+
+</script> 
+<body></body> 
+</html>
